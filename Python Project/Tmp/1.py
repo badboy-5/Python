c5447272c57731cb787420ca305f0f9b90b5a240
Badboy <61783742+badboy-5@users.noreply.github.com>
2024-01-31 09:26:56 +0800

    Delete Python Project/Tmp directory

--- Python Project/Tmp/1.py
+++
@@ -1,8 +0,0 @@
-def get_intf(line):
-    name, *others, bw = line.split()
-    name, intf_state, line_state, _, mtu, bw = line.split()
-
-with open(r'.\Device.log', 'r', encoding='utf8', errors='ignore') as f:
-    log = f.readlines()
-    for line in log:
-        print(line.strip())
